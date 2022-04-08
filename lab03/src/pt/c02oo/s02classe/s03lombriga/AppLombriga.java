05b988e35a25ac48c0c81dcab906ff1a1870d14c
Luiz Henrique Yuji Delgado Oda <l247255@dac.unicamp.br>
2022-04-08 11:48:48 -0300

    conserta AppLombriga.java

--- lab03/src/pt/c02oo/s02classe/s03lombriga/AppLombriga.java
+++ lab03/src/pt/c02oo/s02classe/s03lombriga/AppLombriga.java
@@ -4,20 +4,16 @@ public class AppLombriga {
 
    public static void main(String[] args) {
       Toolkit tk = Toolkit.start();
-      
       String lombrigas[] = tk.recuperaLombrigas();
-      
-      for (int l = 0; l < lombrigas.length; l++)
-         System.out.println(lombrigas[l]);
-      
-      tk.gravaPasso("=====");
-      tk.gravaPasso("##O@@@##");
-      tk.gravaPasso("#O@@@###");
-      tk.gravaPasso("#O@@@@##");
-      tk.gravaPasso("O@@@@###");
-      tk.gravaPasso("@@@@O###");
-      tk.gravaPasso("#@@@@O##");
-      
+      for (int l = 0; l < lombrigas.length; l++) {
+    	  Animacao anima = new Animacao(lombrigas[l]);
+    	  tk.gravaPasso("=====");
+    	  tk.gravaPasso(anima.apresenta());
+    	  for(int j = anima.cur; j < anima.seq.length(); j++) {
+    		  anima.passo();
+    		  tk.gravaPasso(anima.apresenta());
+    	  }
+      }
       tk.stop();
    }
 
